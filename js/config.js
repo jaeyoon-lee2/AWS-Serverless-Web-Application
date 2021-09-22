bcbbed2aa5639dec19af5bd717affd4148974cb4
jaeyoon-lee2 <jaeyoon.lee@mths.ca>
2021-09-22 20:20:31 +0000

    add sign page

---
+++ js/config.js
@@ -0,0 +1,7 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_k49D32veG', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-1', // e.g. us-east-2
+		clientId: '2b653mmhimnhsu9hbmn4che8tl' //is this used anywhere?
+    },
+};
