e7d34e9ab11744a92ca7e21be6152887b354a3b1
jaeyoon-lee2 <jaeyoon.lee@mths.ca>
2021-09-27 20:39:22 +0000

    Update html files

---
+++ js/home.js
@@ -0,0 +1,12 @@
+// JavaScript File
+
+function loadFriendList(friend_list) {
+  for (const friend of friend_list) {
+    var img = document.createElement("img");
+    var friend_name = document.createElement("div");
+    var node = document.createElement("LI");
+    var textnode = document.createTextNode(friend['name']);
+    friend_name.appendChild(textnode);
+    document.getElementById("friend-list").appendChild(node);
+  }
+}
