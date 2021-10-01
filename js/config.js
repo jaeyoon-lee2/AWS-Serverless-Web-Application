bff2244c2e4e466a4a708f491f5600f061d83874
jaeyoon-lee2 <jaeyoon.lee@mths.ca>
2021-10-01 23:01:05 +0000

    Update Maps & add event pages

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_k49D32veG', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-1_HgL5LNxFH', // e.g. us-east-2_uXboG5pAb
         region: 'us-east-1', // e.g. us-east-2
-		clientId: '2b653mmhimnhsu9hbmn4che8tl' //is this used anywhere?
+		clientId: '4ohiu9e8272opkkhjm4p0uvl3h' //is this used anywhere?
     },
 };
